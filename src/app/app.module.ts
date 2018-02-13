8a51228234d5a26f58d2f5bece404973d269695b
Daniel Lebrecht <dlebrecht@gmail.com>
2018-02-13 14:39:55 +0000

    cleaning up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { MinFeeService } from './min-fee.service'
 import { BtcUsdService } from './btc-usd.service'
 import { BlockDetectorService } from './block-detector.service'
 import { WampConnectorService } from './wamp-connector.service'
-import { ChartistModule } from 'ng-chartist'
 import { HttpModule } from '@angular/http'
 import { AboutComponent } from './about/about.component'
 import { AppRoutingModule } from './app-routing.module';
@@ -23,7 +22,6 @@ import { HashLocationStrategy, LocationStrategy } from '@angular/common/';
   ],
   imports: [
     BrowserModule,
-    ChartistModule,
     HttpModule,
     AppRoutingModule,
   ],
