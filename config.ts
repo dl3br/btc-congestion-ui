beb2d9662da99fae46affc2ecb7cea653f9e5e68
Daniel Lebrecht <dlebrecht@gmail.com>
2018-02-27 20:06:28 +0100

    going back one commit

--- config.ts
+++ config.ts
@@ -3,7 +3,11 @@ const url = window.location.host
 export const config = {
   url,
   wamp: {
+<<<<<<< HEAD
     url: `wss://${url}:8080/ws`,
+=======
+    url: `ws://${url}:8080/ws`,
+>>>>>>> d117b3a... change port to 8080 for ws
     realm: 'realm1',
   },
 }
